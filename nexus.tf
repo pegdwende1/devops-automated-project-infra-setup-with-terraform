050b330b5edd35949a1716ac50127bb6deb2a9f1
pegdwende1 <136675538+pegdwende1@users.noreply.github.com>
2024-04-14 21:22:44 -0500

    Update nexus.tf

--- nexus.tf
+++ nexus.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nexus" {
-  ami           = " amazon linux 2 ami"
+  ami           = "ami-050c61fa1bff25e5a"    #amazon linux 2 ami
   instance_type = "t2.medium"
   user_data = base64encode(file("nexus.sh"))
   key_name = "remote-kp"
