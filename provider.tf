d32c6f9424860c7a6622928d4ba20c2e7be2d97f
pegdwende1 <136675538+pegdwende1@users.noreply.github.com>
2024-04-14 21:19:59 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-1b"
   map_public_ip_on_launch = true
 }
